bc7374abafaf7549318fe4974d396fb9ecf4d8f6
Navodya16 <prabuddhi.rath2@gmail.com>
2024-07-10 02:47:04 +0530

    modified index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["https://your-frontend-domain.vercel.app"],
+    origin: ["https://mern-stack-todo-app-frontend-tau.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
